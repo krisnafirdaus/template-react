2a24b2a6bd08c1158759792f9d31f066e1428fae
krisnafirdaus <krisnafirdaus@gmail.com>
2022-05-09 11:18:38 +0800

    add feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello Holiday is Over!!</h1>;
+  const user = {
+    avatarUrl: "https://www.w3schools.com/tags/img_girl.jpg",
+  };
+
+  const name = "andi";
+
+  const element = (
+    <div tabIndex="0">
+      <h1 className="title">Halo!</h1>
+      <h2>Senang melihatmu di sini.</h2>
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <h1>Halo, nama saya {name}</h1>
+      <p>{2 + 2}</p>
+      <img src={user.avatarUrl} />
+      {element}
+    </div>
+  );
 };
 
 export default App;
